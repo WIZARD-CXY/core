2b230f1db1df057edcec026fe0c7171db5cd701d
Lukas Reschke <lukas@statuscode.ch>
2013-05-09 12:21:33 +0200

    Move data to head since we have multiple HTML tags

--- core/templates/layout.user.php
+++ core/templates/layout.user.php
@@ -4,8 +4,8 @@
 <!--[if IE 8]><html class="ng-csp ie ie8 lte9 lte8"><![endif]-->
 <!--[if IE 9]><html class="ng-csp ie ie9 lte9"><![endif]-->
 <!--[if gt IE 9]><html class="ng-csp ie"><![endif]-->
-<!--[if !IE]><!--><html class="ng-csp" data-user="<?php p($_['user_uid']); ?>" data-requesttoken="<?php p($_['requesttoken']); ?>"><!--<![endif]-->
-	<head>
+<!--[if !IE]><!--><html class="ng-csp"><!--<![endif]-->
+	<head data-user="<?php p($_['user_uid']); ?>" data-requesttoken="<?php p($_['requesttoken']); ?>">
 		<title><?php p(!empty($_['application'])?$_['application'].' | ':'') ?>ownCloud
 			<?php p(trim($_['user_displayname']) != '' ?' ('.$_['user_displayname'].') ':'') ?></title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
